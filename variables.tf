9c170791ec8632a7cdc2f1f9338e2263ffdeddb2
vilelf <vilelf.dev@gmail.com>
2021-06-28 01:58:04 -0300

    Adicionando cluster ECS

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "ecr_repo_name" {
   default = false
   description = "Nome do repositorio ECR que será criado"
 }
+
+variable "ecs_cluster_name" {
+  type    = string
+  default = false
+  description = "Nome do cluster ECS que será criado"
+}
