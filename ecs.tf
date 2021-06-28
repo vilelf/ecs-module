3974f7640a80c3a69b5ed8e1e4a01c05872c18ee
vilelf <vilelf.dev@gmail.com>
2021-06-28 02:22:08 -0300

    Adicionando task definition

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,47 @@
 resource "aws_ecs_cluster" "cluster" {
   name = var.ecs_cluster_name
 }
+
+resource "aws_ecs_task_definition" "task" {
+  container_definitions = jsonencode([
+    {
+      "name": "my-first-task",
+      "image": "${aws_ecr_repository.ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ])
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc" 
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
