91e9b8e725651d14a6c8cce9828f1f41b5bde506
vilelf <vilelf.dev@gmail.com>
2021-07-04 21:27:38 -0300

    Remove zone

--- route53.tf
+++ route53.tf
@@ -1,7 +1,3 @@
-data "aws_route53_zone" "selected" {
-  name = "fabricio.stdev.xyz"
-}
-
 resource "aws_route53_record" "domain" {
   zone_id = var.zone_id
   name    = format("%s.%s", var.subdomain_name, data.aws_route53_zone.selected.name)
