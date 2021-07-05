ca3bf8a1d8403dd4abab669a97a66eb190cf3eca
vilelf <vilelf.dev@gmail.com>
2021-07-04 21:17:54 -0300

    Aponta para o alb

--- route53.tf
+++ route53.tf
@@ -4,5 +4,12 @@ data "aws_route53_zone" "selected" {
 
 resource "aws_route53_record" "domain" {
   zone_id = data.aws_route53_zone.zone_id
-  name = format("%s.%s", var.subdomain_name, data.aws_route53_zone.selected.name)
+  name    = format("%s.%s", var.subdomain_name, data.aws_route53_zone.selected.name)
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.application_load_balancer.dns_name
+    zone_id                = aws_lb.application_load_balancer.zone_id
+    evaluate_target_health = true
+  }
 }
