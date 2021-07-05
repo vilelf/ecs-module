c6d598c51067efeaacf8f98f70a9086071e71a6e
vilelf <vilelf.dev@gmail.com>
2021-07-04 21:21:57 -0300

    Passa zone_id como variavel

--- route53.tf
+++ route53.tf
@@ -3,7 +3,7 @@ data "aws_route53_zone" "selected" {
 }
 
 resource "aws_route53_record" "domain" {
-  zone_id = data.aws_route53_zone.zone_id
+  zone_id = var.zone_id
   name    = format("%s.%s", var.subdomain_name, data.aws_route53_zone.selected.name)
   type    = "A"
 
